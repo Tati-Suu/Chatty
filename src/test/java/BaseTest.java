587fdf7eda6bbe5065524bd52346c52b647f838d
Trinity <alles.aug.2000@gmail.com>
2024-08-18 17:48:36 +0200

    Work 180824

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,34 @@
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static com.codeborne.selenide.Selenide.open;
+
 public class BaseTest {
+    @BeforeEach
+    public void setUp(){
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        open("http://chatty.telran-edu.de:8089/login");
+
+    }
+
+    @AfterEach
+    public void tearDown() {
+        closeWebDriver();
+    }
+
+    LoginPage loginPage = new LoginPage();
+
+
+    AdminPage adminPage = new AdminPage();
+
+    AdminAuthPage adminAuthPage = new AdminAuthPage();
+    HomePage homePage = new HomePage();
+
+    CreateAccountPage createAccountPage = new CreateAccountPage();
+
 }
