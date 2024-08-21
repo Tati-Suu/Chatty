eee8aae3a833b27b27140414f4d80b6569f87351
Mariia <hirsch.mariia@icloud.com>
2024-08-21 17:03:17 +0200

    UserProfileEditPage + UserProfileEditTest now passed 21.08.2024

--- src/main/java/UserProfileEditPage.java
+++ src/main/java/UserProfileEditPage.java
@@ -1,2 +1,66 @@
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
 public class UserProfileEditPage {
+
+    private SelenideElement editButtonPlus = $("[data-test='post-header__plus']");
+    private SelenideElement sideBarLeftSection = $x("//*[@class='sidebar__section']");
+    private SelenideElement suggestionsSection = $x("//*[@class='suggestions__section']");
+    private SelenideElement nameField = $x("//input[@name='name']");
+    private SelenideElement surnameField = $x("//input[@name='surname']");
+    private SelenideElement genderField = $("select[data-test='profileGender']");
+    private SelenideElement birthDateField = $("#birthDate");
+    private SelenideElement phoneField = $x("//input[@name='phone']");
+    private SelenideElement changePassButton = $x("//button[@type='button']");
+    private SelenideElement saveButton = $x("//button[@type='submit']");
+    private SelenideElement headerEditCancelWordNearButtonPlus = $x("//*[@class='post-header__plus-box']");
+    private SelenideElement userAvatarUploededPhoto = $x("//img[@data-test='uploaded-image']");
+    private SelenideElement profileEmailUnderAvatar = $x("//p[@data-test='profileEmail']");
+    private SelenideElement userMenu = $x("//p[contains(text(), 'Hello,')]");
+    private SelenideElement profileLink = $x("//a[@href='/userprofile' and text()='Your Profile']");
+    private SelenideElement visibleProfilePage = $x("//a[@href='/userprofile' and text()='Your Profile']");
+
+
+    public SelenideElement getEditButtonPlus() {
+        return editButtonPlus;
+    }
+    public SelenideElement getSideBarLeftSection() {
+        return sideBarLeftSection;
+    }
+    public SelenideElement getSuggestionsSection() {
+        return suggestionsSection;
+    }
+    public SelenideElement getNameField() {
+        return nameField;
+    }
+    public SelenideElement getSurnameField() {
+        return surnameField;
+    }
+    public SelenideElement getGenderField() {
+        return genderField;
+    }
+    public SelenideElement getBirthDateField() {
+        return birthDateField;
+    }
+    public SelenideElement getPhoneField() {
+        return phoneField;
+    }
+    public SelenideElement getChangePassButton() {
+        return changePassButton;
+    }
+    public SelenideElement getSaveButton() {
+        return saveButton;
+    }
+    public SelenideElement getUserMenu() {
+        return userMenu;
+    }
+    public SelenideElement getProfileLink() {
+        return profileLink;
+    }
+    public SelenideElement getVisibleProfilePage() {
+        return visibleProfilePage;
+    }
+
 }
--- src/test/java/UserProfileEditTest.java
+++ src/test/java/UserProfileEditTest.java
@@ -1,9 +1,10 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
 
 public class UserProfileEditTest extends BaseTest {
     @BeforeEach
@@ -17,12 +18,43 @@ public class UserProfileEditTest extends BaseTest {
                 .enterPassword("Blabla2024!")
                 .clickButton();
     }
-//    @Test
-//    public void EditingUserProfileData() {
-//        UserProfileEditPage userProfileEditPage = new UserProfileEditPage();
-//
-//        sleep(5000);
-//
-//    }
+    @Test
+    public void EditingUserProfileData() {
+        UserProfileEditPage userProfileEditPage = new UserProfileEditPage();
+
+        // Ожидание и клик по меню пользователя "Hello, ..."
+        userProfileEditPage.getUserMenu().shouldBe(visible).click();
+
+        // Ожидание и клик по ссылке на профиль
+        userProfileEditPage.getProfileLink().shouldBe(visible, visible).click();
+
+        // Ожидание появления кнопки "+"
+        userProfileEditPage.getEditButtonPlus().shouldBe(visible).click();
+
+        // Ожидание видимости поля имени и ввод нового значения
+        userProfileEditPage.getNameField().shouldBe(visible).setValue("Mariia");
+
+        // Ожидание видимости поля фамилии и ввод нового значения
+        userProfileEditPage.getSurnameField().shouldBe(visible).setValue("Gerasimova");
+
+        // Выбор пола
+        userProfileEditPage.getGenderField().selectOptionByValue("FEMALE");
+
+        // Ввод даты рождения
+        userProfileEditPage.getBirthDateField().setValue("21.03.1992");
+
+        // Ввод телефона
+        userProfileEditPage.getPhoneField().setValue("+12536425857");
+
+        // Сохранение изменений
+        userProfileEditPage.getSaveButton().click();
+
+        // Ожидание и проверка внесенных изменений
+        userProfileEditPage.getNameField().shouldHave(Condition.value("Mariia"));
+        userProfileEditPage.getSurnameField().shouldHave(Condition.value("Gerasimova"));
+        userProfileEditPage.getGenderField().getSelectedOption().shouldHave(Condition.text("Female"));
+        userProfileEditPage.getBirthDateField().shouldHave(Condition.value("1992-03-21"));
+        userProfileEditPage.getPhoneField().shouldHave(Condition.value("+12536425857"));
+    }
 
 }
